e0594e2f7d802a61bdccbbf9e7ccf1a225ff570c
naumovHellsCoder <naumov0coder@gmail.com>
2022-12-18 20:50:44 +0300

    new commit

---
+++ user-service/src/main/java/com/naumov/userservice/entity/User.java
@@ -0,0 +1,29 @@
+package com.naumov.userservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotNull
+    private String username;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+}
---
+++ user-service/src/main/java/com/naumov/userservice/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.naumov.userservice.mapper;
+
+import com.naumov.userservice.entity.User;
+
+public interface UserMapper {
+    User userFromUserDto();
+}
