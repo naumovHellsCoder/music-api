c6d4bddd012eed7c53930040d8c6fa278587e5fc
naumovHellsCoder <naumov0coder@gmail.com>
2023-01-04 01:38:27 +0300

    auth-functional

---
+++ music-service/src/main/java/com/naumov/musicservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.naumov.musicservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .mvcMatcher("/resource/**")
+                .authorizeRequests()
+                .mvcMatchers("/resource/**")
+                .access("hasAuthority('SCOPE_resource.read')")
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
