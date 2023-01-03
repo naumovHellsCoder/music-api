6ff91cc42ff3cd26ed8b29f3ed2ca9e182c27aca
naumovHellsCoder <naumov0coder@gmail.com>
2023-01-04 01:27:07 +0300

    auth-functional

---
+++ music-service/src/main/java/com/naumov/musicservice/controller/MusicController.java
@@ -0,0 +1,16 @@
+package com.naumov.musicservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/music")
+public class MusicController {
+
+    @GetMapping(path = "/")
+    @ResponseStatus(HttpStatus.OK)
+    public String test(){
+        return "test is authenticated";
+    }
+
+}
