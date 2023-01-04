209bd231f4896620ee76fb1cb44222106ce17b08
naumovHellsCoder <naumov0coder@gmail.com>
2023-01-05 00:05:14 +0300

    auth-functional

---
+++ user-service/src/main/java/com/naumov/userservice/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.naumov.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String username;
+    private String email;
+    private String password;
+}
